994a311d14cb3a712631db367a4bc77f65fdbb1e
mhawryluk <marcinhawrylukxx@gmail.com>
2021-05-28 11:50:21 +0200

    implemented saving data to file and reading from it
    not ideal yet, but works

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,23 @@
 import engine.ExpenseCategory._
 import engine.IncomeCategory._
+
 import engine._
 import gui._
 
 object Main extends App{
-//  val tracker = Tracker()
+  val tracker = Tracker()
 //  tracker.addEntry("-2.78", category=House)
 //  tracker.addEntry("-500.99", category=OtherExpense)
 //  tracker.addEntry("-0.88", category=OtherExpense)
-//  tracker.addEntry("500", category=OtherIncome)
-//  println(tracker.expenses)
-//  println(tracker.incomes)
-//
+//  tracker.addEntry("500", category=Work)
+  println(tracker.expenses)
+  println(tracker.incomes)
+  println(tracker.entries)
+
 //  println(tracker.getBetweenDates("2021-01-11", "2021-05-27"))
 //  println(tracker.getSum())
 //  println(tracker.getFromCategories(Set(OtherExpense, OtherIncome)))
-//
+
 //  tracker.removeEntry(1)
 //  println(tracker.expenses)
 //  println(tracker.incomes)
@@ -25,5 +27,4 @@ object Main extends App{
 
   val gui = new MainWindow
   gui.visible = true
-
 }
